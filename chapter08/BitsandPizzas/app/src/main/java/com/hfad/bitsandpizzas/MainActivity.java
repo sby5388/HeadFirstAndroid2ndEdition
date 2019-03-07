26af584adf84b7caea88e814576ce023ad453644
shenbingyan <shenby@server03>
2019-03-07 19:25:37 +0800

    add ch8 use ShareActionProvider as Menu to share 分享工具的使用

--- chapter08/BitsandPizzas/app/src/main/java/com/hfad/bitsandpizzas/MainActivity.java
+++ chapter08/BitsandPizzas/app/src/main/java/com/hfad/bitsandpizzas/MainActivity.java
@@ -1,16 +1,20 @@
 package com.hfad.bitsandpizzas;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private ShareActionProvider shareActionProvider;
+    private SearchView mSearchView;
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        mTextView = (TextView) findViewById(R.id.text);
     }
 
     @Override
@@ -27,7 +32,24 @@ public class MainActivity extends AppCompatActivity {
         MenuItem menuItem = menu.findItem(R.id.action_share);
         shareActionProvider =
                 (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
-        setShareActionIntent("Want to join me for pizza?");
+        setShareActionIntent("Want to join me for pizza?" + mTextView.getText().toString());
+        //TODO 类似于在Menu中使用SearchView，
+        //TODO 1.在menu文件中，需要指定类
+        //TODO 2.然后getActionView类型装换获取相应的
+//        MenuItem searchMenu = menu.findItem(R.id.search_view);
+//        mSearchView = (SearchView) searchMenu.getActionView();
+//        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+//            @Override
+//            public boolean onQueryTextSubmit(String query) {
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean onQueryTextChange(String newText) {
+//                mTextView.setText(newText);
+//                return true;
+//            }
+//        });
         return super.onCreateOptionsMenu(menu);
     }
 
--- chapter08/BitsandPizzas/app/src/main/java/com/hfad/bitsandpizzas/OrderActivity.java
+++ chapter08/BitsandPizzas/app/src/main/java/com/hfad/bitsandpizzas/OrderActivity.java
@@ -1,10 +1,14 @@
 package com.hfad.bitsandpizzas;
 
+import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
+/**
+ * @author Administrator
+ */
 public class OrderActivity extends AppCompatActivity {
 
     @Override
@@ -15,5 +19,16 @@ public class OrderActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // TODO: 2019/3/7  这样写不好的地方在于：通过某一个接入口(非MainActivity 进入该活动时，返回键或者homeUp时
+        // 不一定会回到MainActivity，而是关闭当前，返回调用者的界面
+        if (android.R.id.home == item.getItemId()) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
