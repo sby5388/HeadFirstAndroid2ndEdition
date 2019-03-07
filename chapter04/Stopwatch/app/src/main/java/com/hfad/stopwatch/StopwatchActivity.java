424d7f6248cf91c20563c8fd0205485bcf663a7f
shenbingyan <shenby@server03>
2019-03-07 16:41:16 +0800

    add ch4 use instanceState to save dataStatus when screen has recreate 使用savedInstanceState() .putXX(K,V)来保存活动当前的某些状态值， 以及在onCreate之中getXX(K)来恢复数据； 使用多种方法实现了秒表的功能： 信号的生成（每个多久发送一个信号） 1.Handler.post(Runnable) 2.Timer 3.rxJava. 4.AsyncTask.publish 5.new Thread
    更新UI：
    1.Handler.sendMessage(Message)
    2.activity.runOnUiThread(Runnable)
    3.rxJava.interval(,)
    等

--- chapter04/Stopwatch/app/src/main/java/com/hfad/stopwatch/StopwatchActivity.java
+++ chapter04/Stopwatch/app/src/main/java/com/hfad/stopwatch/StopwatchActivity.java
@@ -1,29 +1,63 @@
 package com.hfad.stopwatch;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import java.lang.ref.WeakReference;
 import java.util.Locale;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
 
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
+
+/**
+ * 秒表功能
+ *
+ * @author Administrator
+ */
 public class StopwatchActivity extends Activity {
     private int seconds = 0;
     private boolean running;
     private boolean wasRunning;
+    private MyTimerTask mTask;
+    private TextView mTextView;
+    private Disposable mDisposable;
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
+        mTextView = (TextView) findViewById(R.id.time_view);
+        mTask = new MyTimerTask();
+
         if (savedInstanceState != null) {
             seconds = savedInstanceState.getInt("seconds");
             running = savedInstanceState.getBoolean("running");
             wasRunning = savedInstanceState.getBoolean("wasRunning");
         }
-        runTimer();
+        //todo use Handle.post(Runnable,delay)
+        //runTimer();
+        //todo use timer
+        //new Timer().schedule(mTask, 0, 1000);
+        //todo use asyncTask
+        //new MyAsyncTask(this).execute();
+        // todo use rxJava
+        //useRxJava();
+        //todo use sub_thread
+        useSubThread();
+
     }
+
 
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
@@ -47,40 +81,156 @@ public class StopwatchActivity extends Activity {
         running = false;
     }
 
-    //Start the stopwatch running when the Start button is clicked.
+
     public void onClickStart(View view) {
         running = true;
     }
 
-    //Stop the stopwatch running when the Stop button is clicked.
     public void onClickStop(View view) {
         running = false;
     }
 
-    //Reset the stopwatch when the Reset button is clicked.
     public void onClickReset(View view) {
         running = false;
         seconds = 0;
     }
 
-    //Sets the number of seconds on the timer.
+    /**
+     * 使用Handler+Runnable()
+     */
+    @SuppressWarnings("unused")
     private void runTimer() {
-        final TextView timeView = (TextView)findViewById(R.id.time_view);
         final Handler handler = new Handler();
         handler.post(new Runnable() {
             @Override
             public void run() {
-                int hours = seconds/3600;
-                int minutes = (seconds%3600)/60;
-                int secs = seconds%60;
-                String time = String.format(Locale.getDefault(),
-                        "%d:%02d:%02d", hours, minutes, secs);
-                timeView.setText(time);
-                if (running) {
-                    seconds++;
-                }
+                String time = getShowText();
+                mTextView.setText(time);
                 handler.postDelayed(this, 1000);
             }
         });
+
+    }
+
+    private String getShowText() {
+        int hours = seconds / 3600;
+        int minutes = (seconds % 3600) / 60;
+        int secs = seconds % 60;
+        String time = String.format(Locale.getDefault(), "%d:%02d:%02d", hours, minutes, secs);
+        if (running) {
+            seconds++;
+        }
+        return time;
+    }
+
+    /**
+     * 使用Timer+TimerTask+Handler()
+     */
+    @SuppressWarnings("unused")
+    private class MyTimerTask extends TimerTask {
+        MyTimerTask() {
+        }
+
+        @Override
+        public void run() {
+            final String time = getShowText();
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    mTextView.setText(time);
+                }
+            });
+        }
+    }
+
+    /**
+     * 使用不安全的AsyncTask
+     */
+    @SuppressWarnings("unused")
+    private static class MyAsyncTask extends AsyncTask<Void, Integer, Void> {
+        private static final String TAG = "MyAsyncTask";
+        WeakReference<StopwatchActivity> mWeakReference;
+        boolean running = true;
+
+        MyAsyncTask(StopwatchActivity activity) {
+            mWeakReference = new WeakReference<>(activity);
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            final StopwatchActivity activity = this.mWeakReference.get();
+            if (activity != null) {
+
+                final String text = activity.getShowText();
+                activity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        activity.mTextView.setText(text);
+                    }
+                });
+            } else {
+                running = false;
+                this.cancel(false);
+            }
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            while (running) {
+                publishProgress(0);
+                try {
+                    Thread.sleep(1000);
+                } catch (Exception e) {
+                    Log.e(TAG, "doInBackground: ", e);
+                }
+            }
+            return null;
+        }
+    }
+
+    @SuppressWarnings("all")
+    private void useRxJava() {
+        mDisposable = Observable.interval(1, TimeUnit.SECONDS)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<Long>() {
+                    @Override
+                    public void accept(Long aLong) throws Exception {
+                        final String text = getShowText();
+                        mTextView.setText(text);
+                    }
+                });
+    }
+
+    @SuppressWarnings("all")
+    private void useSubThread() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    final String text = getShowText();
+                    //could replace next method with Handle.sendMessage
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            mTextView.setText(text);
+                        }
+                    });
+                    try {
+                        Thread.sleep(1000);
+                    } catch (Exception e) {
+
+                    }
+                }
+            }
+        }).start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mDisposable != null && !mDisposable.isDisposed()) {
+            mDisposable.dispose();
+        }
     }
 }
